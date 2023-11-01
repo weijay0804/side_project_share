a0771f62e8aa0b1c3b919c57d7253a9f6aa9e38d
weijay <weijay0804@gmail.com>
2023-11-02 00:32:18 +0800

    refactor(code): 重構 User schemas
    將 User schemas 改成不用繼承的方式，而是直接針對每個 model 去寫，因為發現用繼承的方式會有點混亂。
    
    re gh-6

--- backend/app/schemas/api_schemas/user.py
+++ backend/app/schemas/api_schemas/user.py
@@ -1,25 +1,49 @@
-from typing import Optional
+from typing import Optional, List
 from pydantic import BaseModel
 
-from app.schemas.db_schemas.user import UserDBCreaet, UserInDB, UserDBUpdate
+
+class User(BaseModel):
+    """使用者資源詳細資料"""
+
+    id: int
+    username: str
+    email: str
+    avatar_url: Optional[str] = None
+    city: Optional[str] = None
+    age: Optional[str] = None
+    gender: Optional[str] = None
+    is_email_public: bool
+    github: Optional[str] = None
+    is_github_public: bool
+    discord: Optional[str] = None
+    is_discord_public: bool
+    skill: Optional[List[str]] = None
 
 
-class User(UserInDB):
-    """使用者資源資料"""
-
-    pass
-
-
-class UserCreate(UserDBCreaet):
+class UserCreate(BaseModel):
     """建立使用者資源"""
 
-    pass
+    username: str
+    email: str
+    password: str
 
 
-class UserUpdate(UserDBUpdate):
+class UserUpdate(BaseModel):
     """更新使用者資源"""
 
-    pass
+    username: Optional[str] = None
+    email: Optional[str] = None
+    password: Optional[str] = None
+    avatar_url: Optional[str] = None
+    city: Optional[str] = None
+    age: Optional[str] = None
+    gender: Optional[str] = None
+    is_email_public: Optional[bool] = None
+    github: Optional[str] = None
+    is_github_public: Optional[bool] = None
+    discord: Optional[str] = None
+    is_discrod_public: Optional[bool] = None
+    skill: Optional[List[str]] = None
 
 
 class UserSimple(BaseModel):
